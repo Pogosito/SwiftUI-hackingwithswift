ac6badc6fb517958f9108d60252195b2962bddd6
Pogos Anesyan <pogosanesyan@gmail.com>
2021-03-03 23:58:27 +0300

    День 26: Узнал про: - Stepper - DatePicker
    Повторил работу с датами

---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Pogos Anesyan on 01.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    @State private var stepperCounter: UInt8 = 17
+    @State private var date = Date()
+    var body: some View {
+        Form {
+            Stepper(value: $stepperCounter) {
+                Text("Your value is \(stepperCounter)")
+            }
+
+            Stepper(value: $stepperCounter, in: 5...15) {
+                Text("Your value is \(stepperCounter)")
+            }
+
+            Stepper(value: $stepperCounter, in: 1...30, step: UInt8.Stride(10) ){
+                Text("Your value is \(stepperCounter)")
+            }
+
+            Section {
+                DatePicker("Date", selection: $date)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
