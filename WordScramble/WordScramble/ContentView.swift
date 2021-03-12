37e00d967d27e4307f2f16beca9faf69c64c64b4
Pogos Anesyan <pogosanesyan@gmail.com>
2021-03-13 00:10:26 +0300

    День 30:
    - Доделал проект WordScramble
    - Повторил модификатор .onAppear

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -9,40 +9,100 @@ import SwiftUI
 
 struct ContentView: View {
 
-    let values = ["Array row 1",
-                  "Array row 1",
-                  "Array row 2",
-                  "Array row 1",
-                  "Array row 3",
-                  "Array row 4"]
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
 
     var body: some View {
-        List {
-            Section(header: Text("Секция 1")) {
-                Text("Static row 1")
-                Text("Static row 2")
-                Text("Static row 3")
-            }
-
-            Section(header: Text("Секция 2")) {
-                ForEach(0..<3) {
-                    Text("Dynamic row \($0)")
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding()
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
                 }
             }
+            .navigationBarTitle(rootWord)
+            .onAppear(perform: startGame)
+            .alert(isPresented: $showingError, content: {
+                Alert(title: Text(errorTitle),
+                      message: Text(errorMessage),
+                      dismissButton: .default(Text("OK")))
+            })
+        }
+    }
 
-            Section(header: Text("Секция 3")) {
-                Text("Static row 4")
-                Text("Static row 5")
-                Text("Static row 6")
-            }
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
 
-            Section(header: Text("Секция 4")) {
-                ForEach(values, id: \.self) {
-                    Text("\($0)")
-                }
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized",
+                      message: "You can't just make them up, you know")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible",
+                      message: "That isn't real word")
+            return
+        }
+
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+
+    func startGame() {
+        if let startWordsUrl = Bundle.main.url(forResource: "start", withExtension: ".txt") {
+            if let startWords = try? String(contentsOf: startWordsUrl) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "slikworm"
+                return
             }
         }
-        .listStyle(SidebarListStyle())
+        fatalError("Could not load start.txt from bundle.")
+    }
+
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord.lowercased()
+        for latter in word {
+            if let pos = tempWord.firstIndex(of: latter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0,
+                            length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
---
+++ WordScramble/WordScramble/Demo.swift
@@ -0,0 +1,47 @@
+//
+//  Demo.swift
+//  WordScramble
+//
+//  Created by Pogos Anesyan on 12.03.2021.
+//
+
+import SwiftUI
+
+struct Demo: View {
+
+    let values = ["Array row 1",
+                  "Array row 1",
+                  "Array row 2",
+                  "Array row 1",
+                  "Array row 3",
+                  "Array row 4"]
+
+    var body: some View {
+        List {
+            Section(header: Text("Секция 1")) {
+                Text("Static row 1")
+                Text("Static row 2")
+                Text("Static row 3")
+            }
+
+            Section(header: Text("Секция 2")) {
+                ForEach(0..<3) {
+                    Text("Dynamic row \($0)")
+                }
+            }
+
+            Section(header: Text("Секция 3")) {
+                Text("Static row 4")
+                Text("Static row 5")
+                Text("Static row 6")
+            }
+
+            Section(header: Text("Секция 4")) {
+                ForEach(values, id: \.self) {
+                    Text("\($0)")
+                }
+            }
+        }
+        .listStyle(SidebarListStyle())
+    }
+}
