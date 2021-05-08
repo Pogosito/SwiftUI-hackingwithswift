e8493134c28250c039d7c95b8609ebc3e508bfd1
Pogos <56889876+Pogosito@users.noreply.github.com>
2021-05-08 12:24:33 +0300

    День 40:
    Сделал первую часть проекта Moonshot.
    
    Из нового использовал только NavigationLink и узнал, что у JSONDecoder есть удобное свойство для обработки дат - dateDecodingStrategy

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+
+	let astronauts: [Astronauts] = Bundle.main.decode("astronauts.json")
+	let missions: [Mission] = Bundle.main.decode("missions.json")
+
 	var body: some View {
-		Text("Привет")
+		NavigationView {
+			List(missions) { mission in
+				NavigationLink(destination: Text("Detail view")) {
+					Image(mission.image)
+						.resizable()
+						.scaledToFit()
+						.frame(width: 44, height: 44)
+
+					VStack(alignment: .leading) {
+						Text(mission.displayName)
+							.font(.headline)
+						Text(mission.formattedLaunchDate)
+					}
+				}
+			}
+			.navigationBarTitle("Moonshot")
+		}
 	}
 }
 
